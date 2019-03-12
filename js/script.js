71c35a447fcb5f83709adfded2af678a005ae7f3
David Blackwell <david1213117@gmail.com>
2019-03-12 09:46:41 +0800

    First commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            $('#qodef-back-to-top').fadeIn();
+        } else {
+            $('#qodef-back-to-top').fadeOut();
+        }
+    });
+    $(document).ready(function() {
+        $("#qodef-back-to-top").click(function(event) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+            return false;
+        });
     
+    });
 })
